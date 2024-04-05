9fae11c3fb6f8b4e1650e1dcf266ad062be1b9b6
112021171 <160687905+112021171@users.noreply.github.com>
2024-04-05 21:49:58 +0800

    Update Sinokuhle.py
    midterm code still yet to be edited and debugged

--- midterm/Sinokuhle.py
+++ midterm/Sinokuhle.py
@@ -1,1 +1,16 @@
-print('Hello World')
+class YearEndOverview:
+    def __init__(self,initial_deposit):
+        self.sales=initial_deposit
+        self.transaction=[]
+    def profit(sales,expenses):
+    sales=float(input())
+    expenses=float(input())
+    profit=sales-expenses
+    return profit 
+match profit:
+    case 4000:
+        print("New margins need to be implemented")
+    case 5000:
+        print("We're doing great,but we can do better")
+    case 6000:
+        print("We're smashing it! Consider investing in other markets.")   
