045bf013629becdc6aee360ef92d79aa72d4c81c
112021171 <160687905+112021171@users.noreply.github.com>
2024-04-16 21:29:51 +0800

    Update Sinokuhle.py
    Code creates csv file in the operating system  and prompts user to enter,edit and remove expenses

--- midterm/Sinokuhle.py
+++ midterm/Sinokuhle.py
@@ -1,39 +1,98 @@
- import pprint
+from dataclasses import dataclass
+import pprint
+from datetime import datetime
+import csv
+import os
 
-class YearEndOverview:
-    def __init__(self, initial_deposit):
-        self.capital = initial_deposit
-        self.yearlyprofits = []
-    
-    def calculate_profit(self, sales, expenses):
-        profit = sales - expenses
-        return profit
-    
-    def calculate_yearly_profit(self):
-        for i in range(12):
-            sales = int(input("Enter sales for the month: "))
-            expenses = int(input("Enter expenses for the month: "))
-            profit = self.calculate_profit(sales, expenses)
-            self.yearlyprofits.append(profit)
-            print(profit)
-    
-    def display_transactions(self):
-        pprint.pprint(self.yearlyprofits)
+@dataclass
+class Expense:
+    date: datetime
+    category: str
+    amount: float
 
-    def check_profit(self):
-        for profit in self.yearlyprofits:
-            match profit:
-                case 4000:
-                    print("New margins need to be implemented")
-                case 5000:
-                    print("We're doing great, but we can do better")
-                case 6000:
-                    print("We're smashing it! Consider investing in other markets.")
-                case _:
-                    print("Your profit is within the normal range.")
+def create_csv_if_not_exists():
+    csv_file_name = 'expenses.csv'
+    if not os.path.isfile(csv_file_name):
+        with open(csv_file_name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Date", "Category", "Amount"])
 
-overview = YearEndOverview(2000)
-overview.calculate_yearly_profit()
-overview.display_transactions()
-overview.check_profit()
-overview.check_profit(self):
+def upload_expense():
+    expenses = []
+    csv_file_name = 'expenses.csv'
+    if os.path.isfile(csv_file_name):
+        with open(csv_file_name, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader) 
+            for row in reader:
+                date = datetime.strptime(row[0], "%Y-%m-%d")
+                category = row[1]
+                amount = float(row[2])
+                expenses.append(Expense(date, category, amount))
+    return expenses
+
+def save_expenses(expenses):
+    csv_file_name = 'expenses.csv'
+    with open(csv_file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Date", "Category", "Amount"])  # Write header row
+        for expense in expenses:
+            writer.writerow([expense.date.strftime("%Y-%m-%d"), expense.category, expense.amount])
+
+print("Finance Tracker")
+menu = ["1. Add expense", "2. Edit expense", "3. Remove expense", "4. Print out expenses"]
+pprint.pprint(menu)
+create_csv_if_not_exists()
+expenses = upload_expense()
+
+while True:
+    print("Please select a function")
+    function = input()
+    match function:
+        case "1":
+            print("Add expense")
+            date = datetime.strptime(input('Please enter date (yy-mm-dd): '), "%y-%m-%d")
+            print("Date: ", date)
+            category = input("Category: ")
+            amount = float(input("Amount: "))
+            new_expense = Expense(date, category, amount)
+            expenses.append(new_expense)
+            save_expenses(expenses)
+        case "2":
+            print("Edit expense")
+            print("Please choose transaction to edit")
+            for i, expense in enumerate(expenses):
+                print(f"{i+1}. Date: {expense.date}, Category: {expense.category}, Amount: {expense.amount}")
+            index = int(input("Enter the index of the expense you want to edit: ")) - 1
+            if 0 <= index < len(expenses):
+                new_category = input("Enter new category: ")
+                new_amount = float(input("Enter new amount: "))
+                expenses[index] = Expense(expenses[index].date, new_category, new_amount)
+                save_expenses(expenses)
+                print("Expense edited successfully!")
+            else: 
+                print("Invalid index")
+        case "3":
+            print("Remove expense")
+            for i, expense in enumerate(expenses):
+                print(f"{i+1}. Date: {expense.date}, Category: {expense.category}, Amount: {expense.amount}")
+            index = int(input("Enter the number of the expense you want to delete: ")) - 1
+            if 0 <= index < len(expenses):
+                del expenses[index]
+                save_expenses(expenses)
+                print("Expense deleted successfully!")
+            else:
+                print("Invalid index")
+        case "4":
+            print("Print expenses")
+            with open('expenses.csv', 'r', newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    print(*row, sep=',')
+        case _:
+              print("Thank you for using the app today!")
+    
+     
+    
+    
+
